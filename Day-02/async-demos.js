ee001653962783e461cf900140807bf17d031cb6
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-25 14:48:21 +0530

    Handling errors in callbacks

--- Day-02/async-demos.js
+++ Day-02/async-demos.js
@@ -32,6 +32,33 @@
         });
     }
 
-    window['addAsyncCallbackClient'] = addAsyncCallbackClient;
+    window['addAsyncCallbackClient'] = divideAsyncCallbackClient;
+
+    //Handling failures (using Error-First-Callback pattern)
+    function divideAsyncCallback(x, y, callbackFn) {
+        console.log(`   [@service] processing ${x} and ${y}`);
+        setTimeout(() => {
+            if (y === 0){
+                const err = new Error('divisor cannot be 0');
+                return callbackFn(err)
+            }
+            const result = x / y
+            console.log(`   [@service] returing result`);
+            callbackFn(null, result);
+        }, 5000);
+    }
+
+    function divideAsyncCallbackClient() {
+        console.log(`[@client] invoking the service`);
+        divideAsyncCallback(200, 0, function (err, result) {
+            if (err) {
+                console.log('something went wrong :', err)
+                return
+            }
+            console.log(`[@client] result = ${result}`)
+        });
+    }
+
+    window['divideAsyncCallbackClient'] = divideAsyncCallbackClient;
 
 })()
