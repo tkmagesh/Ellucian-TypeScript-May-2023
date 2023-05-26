c893f08f83ca741b0826eecbeecaa68ed240c41b
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-26 11:27:43 +0530

    Creating a generic collection - step - 1

--- Day-02/typescript-app/dist/generics-demo.js
+++ Day-02/typescript-app/dist/generics-demo.js
@@ -1,4 +1,29 @@
 "use strict";
+class ProductsCollection {
+    constructor() {
+        this.list = [];
+    }
+    add(item) {
+        this.list.push(item);
+    }
+    getAll() {
+        return [...this.list];
+    }
+    get(idx) {
+        return this.list[idx];
+    }
+    get length() {
+        return this.list.length;
+    }
+    sortById() {
+        for (let i = 0; i < this.list.length - 1; i++)
+            for (let j = 0; j < this.list.length; j++) {
+                if (this.list[i].id > this.list[j].id) {
+                    [this.list[i], this.list[j]] = [this.list[j], this.list[i]];
+                }
+            }
+    }
+}
 class MyCollection {
     constructor() {
         this.list = [];
@@ -9,16 +34,32 @@ class MyCollection {
     get(idx) {
         return this.list[idx];
     }
+    getAll() {
+        return [...this.list];
+    }
     get length() {
         return this.list.length;
     }
 }
-const myNoCol = new MyCollection();
-myNoCol.add(10);
-myNoCol.add(20);
-const myStrCol = new MyCollection();
-myStrCol.add("pen");
-myStrCol.add("pencil");
-const myCol = new MyCollection();
-myCol.add(100);
-myCol.add("Pen");
+/*
+const myNoCol = new MyCollection<number>()
+myNoCol.add(10)
+myNoCol.add(20)
+
+const myStrCol = new MyCollection<string>()
+myStrCol.add("pen")
+myStrCol.add("pencil")
+
+const myCol = new MyCollection<number | string>()
+myCol.add(100)
+myCol.add("Pen")
+*/
+// const products = new ProductsCollection()
+const products = new MyCollection();
+products.add({ id: 6, name: 'Pen', cost: 50, units: 20, category: 'stationary' });
+products.add({ id: 9, name: 'Ten', cost: 70, units: 70, category: 'stationary' });
+products.add({ id: 3, name: 'Len', cost: 60, units: 60, category: 'grocery' });
+products.add({ id: 5, name: 'Zen', cost: 30, units: 30, category: 'grocery' });
+products.add({ id: 1, name: 'Ken', cost: 20, units: 80, category: 'utencil' });
+products.add({ id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' });
+console.table(products.getAll());
--- Day-02/typescript-app/generics-demo.ts
+++ Day-02/typescript-app/generics-demo.ts
@@ -1,12 +1,53 @@
-class MyCollection<T>{
-    private list : T[] = []
+type MyProduct = {
+    id : number,
+    name : string,
+    cost : number,
+    units : number,
+    category : string
+}
 
-    add(item : T){
+class ProductsCollection {
+    private list : MyProduct[] = []
+
+    add(item: MyProduct) {
         this.list.push(item)
     }
 
-    get(idx : number) {
+    getAll() : MyProduct[] {
+        return [...this.list];
+    }
+
+    get(idx: number) : MyProduct {
         return this.list[idx]
+    }
+
+    get length(): number {
+        return this.list.length
+    }
+
+    sortById(){
+        for (let i = 0; i < this.list.length - 1;  i++)
+            for (let j = 0; j < this.list.length; j++){
+                if (this.list[i].id > this.list[j].id){
+                    [this.list[i], this.list[j]] = [this.list[j], this.list[i]]
+                }
+            }
+    }
+}
+
+class MyCollection<T>{
+    private list : T[] = []
+
+    add(item : T) : void {
+        this.list.push(item)
+    }
+
+    get(idx : number): T {
+        return this.list[idx]
+    }
+
+    getAll(): T[] {
+        return [...this.list];
     }
 
     get length() : number{
@@ -14,6 +55,7 @@ class MyCollection<T>{
     }
 }
 
+/* 
 const myNoCol = new MyCollection<number>()
 myNoCol.add(10)
 myNoCol.add(20)
@@ -24,6 +66,20 @@ myStrCol.add("pencil")
 
 const myCol = new MyCollection<number | string>()
 myCol.add(100)
-myCol.add("Pen")
+myCol.add("Pen") 
+*/
+
+// const products = new ProductsCollection()
+const products = new MyCollection<MyProduct>()
+
+products.add({ id: 6, name: 'Pen', cost: 50, units: 20, category: 'stationary' });
+products.add({ id: 9, name: 'Ten', cost: 70, units: 70, category: 'stationary' });
+products.add({ id: 3, name: 'Len', cost: 60, units: 60, category: 'grocery' });
+products.add({ id: 5, name: 'Zen', cost: 30, units: 30, category: 'grocery' });
+products.add({ id: 1, name: 'Ken', cost: 20, units: 80, category: 'utencil' });
+products.add({ id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' });
+
+console.table(products.getAll())
+
 
 
